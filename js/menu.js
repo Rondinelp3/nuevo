ec2aa05deb8ded691975ff987c8cf5f216b0377b
Rondinelp3 <daniel_rondinelp3@hotmail.com>
2020-02-05 23:55:30 -0500

    wow2

--- js/menu.js
+++ js/menu.js
@@ -5,8 +5,26 @@ btnMenu.addEventListener('click', ()=>{
     nav.classList.toggle('mostrar')
 })
 
+function isMobile(){
+    return (
+        (navigator.userAgent.match(/Android/i)) ||
+        (navigator.userAgent.match(/webOS/i)) ||
+        (navigator.userAgent.match(/iPhone/i)) ||
+        (navigator.userAgent.match(/iPod/i)) ||
+        (navigator.userAgent.match(/iPad/i)) ||
+        (navigator.userAgent.match(/BlackBerry/i))
+    );
+}
+
+let valor = 0;
+if (isMobile()) {
+	valor = 150;
+}else{
+	valor = 550;
+}
+
  $(window).scroll(function () {
-    if ($("#header_contenedor").offset().top >850) {
+    if ($("#header_contenedor").offset().top > valor) {
 
     	console.log('llego al scroll');
 
