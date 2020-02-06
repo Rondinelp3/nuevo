cfe4810f894360a95bdbd310e6a70e61c5dc2907
Rondinelp3 <daniel_rondinelp3@hotmail.com>
2020-02-05 23:27:33 -0500

    wow1

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,23 @@
 var btnMenu = document.getElementById('btn-menu');
 var nav = document.getElementById('nav');
 
-btnMenu.addEventListener('click', function(){
+btnMenu.addEventListener('click', ()=>{
     nav.classList.toggle('mostrar')
 })
+
+ $(window).scroll(function () {
+    if ($("#header_contenedor").offset().top >850) {
+
+    	console.log('llego al scroll');
+
+    	 $("#header_contenedor").removeClass("colorPrimario");
+       	 $("#header_contenedor").addClass("contenedor_segundo");
+
+        } else {
+
+            $("#header_contenedor").removeClass("contenedor_segundo");
+            $("#header_contenedor").addClass("colorPrimario");
+ 
+        }
+    
+});
